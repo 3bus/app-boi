b6422296e709aae44f1f3fc49802c96fb06d6be4
Caleb Ion <calebionpersonal@gmail.com>
2022-08-20 11:34:56 +1200

    Improve app design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const Layout = ({ children }: Props) => {
   return (
     <div
-      className={tw`w-screen h-screen flex flex-row sm:justify-center sm:items-center`}
+      className={tw`w-screen h-screen flex flex-row sm:justify-center sm:items-center `}
     >
       <div
         className={tw([
@@ -18,13 +18,13 @@ export const Layout = ({ children }: Props) => {
             max-width: 390px;
             max-height: 844px;
           `,
-          "w-full h-full bg-gray-200 relative",
+          "w-full h-full bg-gray-200 relative rounded-2xl shadow-xl overflow-hidden",
         ])}
       >
         <Header />
         {children}
         <NavigationBar />
-        <MapView/>
+        <MapView />
       </div>
     </div>
   );
