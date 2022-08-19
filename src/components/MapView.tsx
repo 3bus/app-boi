a58d437d02167876cf35dd35971ba43e8539b02e
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-08-20 11:10:49 +1200

    MAPBOX_ACCESS_TOKEN

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,3 +1,6 @@
+
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiaWxpYS10dXJub3V0IiwiYSI6ImNsNzExdnF2NzBrNmkzdm1xYW5zdjBsdzIifQ.t8cMmqUo6Mv2dBTySkkOWg"
+
 export default function MapView() {
     return <div>Map goes here</div>
 }
