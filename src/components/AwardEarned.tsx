1c1f28f8928057107b946aa9c9f4a938893c8dd6
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-08-20 21:34:40 +1200

    discount circle

--- src/components/AwardEarned.tsx
+++ src/components/AwardEarned.tsx
@@ -1,6 +1,10 @@
 import { Layout } from "./Layout";
 import { FaCheck } from "react-icons/fa";
 
+function adjuster(num: number) {
+  return (num / 428) * 390;
+}
+
 export default function AwardEarned() {
   return (
     <Layout hasSearch={false}>
@@ -17,22 +21,56 @@ export default function AwardEarned() {
           height: 597,
           width: 597,
           alignItems: "center",
-          justifyContent: 'flex-end',
+          justifyContent: "flex-end",
           gap: 10,
           backgroundColor: "rgb(50, 119, 102, 0.6)",
-          marginLeft: 'calc((597px - 390px) / -2)',
-          marginTop: 'calc(-590px + 150px)',
-          borderBottomLeftRadius: '100%',
-          borderBottomRightRadius: '100%'
+          marginLeft: "calc((597px - 390px) / -2)",
+          marginTop: "calc(-590px + 150px)",
+          borderBottomLeftRadius: "100%",
+          borderBottomRightRadius: "100%",
         }}
       >
-        <div style={{fontSize: 24, paddingTop: 40, color: 'white'}}>Great Job!</div>
-        <div style={{paddingBottom: 40}}>
-          <FaCheck className="text-green-500" size={49/428*390} color="white" />
+        <div style={{ fontSize: 24, paddingTop: 40, color: "white" }}>
+          Great Job!
+        </div>
+        <div style={{ paddingBottom: 40 }}>
+          <FaCheck
+            className="text-green-500"
+            size={(49 / 428) * 390}
+            color="white"
+          />
         </div>
       </div>
-      <div>Discount</div>
-      <div>20%</div>
+      <div
+        style={{
+          background: "rgb(254, 255, 215)",
+          borderRadius: "100%",
+          width: adjuster(295),
+          height: adjuster(295),
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            background: "#F9FFB3",
+            borderRadius: "100%",
+            width: adjuster(230),
+            height: adjuster(230),
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <div>
+            <span style={{ fontSize: adjuster(64), lineHeight: adjuster(64) + "px", fontWeight: '500' }}>20</span>
+            <span style={{ fontSize: adjuster(40), lineHeight: adjuster(64) + "px", fontWeight: '500' }}>%</span>
+          </div>
+          <div style={{ fontSize: adjuster(24) }}>Discount</div>
+        </div>
+      </div>
       <div>You earned a 20% discount towards any </div>
       <div>AT HOP monthly pass</div>
       <div>Back</div>
