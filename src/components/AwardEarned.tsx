cf1a35584d29ec8cd8c8fe68c12a6b5cb34f24de
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-08-21 10:07:08 +1200

    tidy up AwardEarned component

--- src/components/AwardEarned.tsx
+++ src/components/AwardEarned.tsx
@@ -8,13 +8,13 @@ function adjuster(num: number) {
 }
 
 function getCircleColor(background: string) {
-  var c = background.substring(1); // strip #
-  var rgb = parseInt(c, 16); // convert rrggbb to decimal
-  var r = (rgb >> 16) & 0xff; // extract red
-  var g = (rgb >> 8) & 0xff; // extract green
-  var b = (rgb >> 0) & 0xff; // extract blue
+  const c = background.substring(1); // strip #
+  const rgb = parseInt(c, 16); // convert rrggbb to decimal
+  const r = (rgb >> 16) & 0xff; // extract red
+  const g = (rgb >> 8) & 0xff; // extract green
+  const b = (rgb >> 0) & 0xff; // extract blue
 
-  var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+  const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
 
   if (luma < 40) {
     // pick a different colour
@@ -22,6 +22,7 @@ function getCircleColor(background: string) {
   }
   return "black";
 }
+
 interface Props {
   topLeadCopy: string;
   circleLeadCopy: string;
@@ -86,7 +87,6 @@ export default function AwardEarned(props: Props) {
         </div>
         <div
           style={{
-            // background: "rgb(254, 255, 215)",
             background: circleOutline,
             borderRadius: "100%",
             width: adjuster(295),
@@ -99,7 +99,6 @@ export default function AwardEarned(props: Props) {
         >
           <div
             style={{
-              // background: "#F9FFB3",
               background: circleBackground,
               borderRadius: "100%",
               width: adjuster(230),
@@ -114,7 +113,8 @@ export default function AwardEarned(props: Props) {
             <div style={{ textAlign: "center" }}>
               <span
                 style={{
-                  fontSize: circleLeadCopy.length > 3 ? adjuster(40) : adjuster(64),
+                  fontSize:
+                    circleLeadCopy.length > 3 ? adjuster(40) : adjuster(64),
                   lineHeight: adjuster(64) + "px",
                   fontWeight: "500",
                 }}
