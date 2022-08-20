50e8b2901f75694da81f878a4bb28435023d7ab9
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-08-20 12:10:36 +1200

    add layout for profile

--- src/screens/Account.tsx
+++ src/screens/Account.tsx
@@ -1,5 +1,23 @@
+import { tw } from "twind/css";
 import { Layout } from "../components";
 
+function ProfileSection() {
+  return (
+    <div className={tw`flex flex-col`}>
+      <img src="https://minimaltoolkit.com/images/randomdata/male/49.jpg" />
+      XX Points
+      <div>You have XX points to [reward]</div>
+      <div>Tier level</div>
+    </div>
+  );
+}
+
 export const Account = () => {
-  return <Layout hasSearch={false}></Layout>;
+  return (
+    <Layout hasSearch={false}>
+      {/* TODO: better fix */}
+      <div style={{ paddingTop: 56 }}></div>
+      <ProfileSection />
+    </Layout>
+  );
 };
