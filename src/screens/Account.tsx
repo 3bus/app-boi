6a4a52751c2c54cfcb83d8b29894557f1714462a
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-08-20 15:38:50 +1200

    some good tailwind layout for profile

--- src/screens/Account.tsx
+++ src/screens/Account.tsx
@@ -7,70 +7,103 @@ function SectionContainer({ children }: { children: React.ReactNode }) {
   return <div className={tw`p-6`}>{children}</div>;
 }
 
-interface ProfileSectionProps {
+interface ProfileAccountProps {
   pointCount: number;
   rewardPoints: number;
   nextReward: string;
 }
 
-function ProfileSection(props: ProfileSectionProps) {
+function ProfileAccount(props: ProfileAccountProps) {
   const percentage = props.pointCount / props.rewardPoints;
-  const navigate = useNavigate();
-  const [opened, setOpened] = useState(false);
   return (
-    <div>
+    <div className={tw`flex flex-col bg-white rounded-xl items-center gap-2 w-full`}>
+      <img
+        src="https://minimaltoolkit.com/images/randomdata/male/49.jpg"
+        className={tw`rounded-full w-24`}
+      />
       <div
-        className={tw`flex flex-col px-4 py-6 bg-white rounded-xl items-center gap-2	`}
+        className={tw(
+          `h-4 rounded-full bg-gray-300`,
+          css`
+            width: calc(390px * 0.6);
+          `
+        )}
       >
-        <img
-          src="https://minimaltoolkit.com/images/randomdata/male/49.jpg"
-          className={tw`rounded-full w-24`}
-        />
         <div
           className={tw(
-            `h-4 rounded-full bg-gray-300`,
+            `h-4 rounded-full bg-blueGray-700`,
             css`
-              width: calc(390px * 0.6);
+              width: calc(390px * 0.6 * ${percentage});
             `
           )}
-        >
-          <div
-            className={tw(
-              `h-4 rounded-full bg-blueGray-700`,
-              css`
-                width: calc(390px * 0.6 * ${percentage});
-              `
-            )}
-          ></div>
+        ></div>
+      </div>
+      <div className={tw`font-semibold`}>{props.pointCount} Points</div>
+      <div className={tw`text-sm opacity-60`}>
+        You have {props.rewardPoints - props.pointCount} points to{" "}
+        {props.nextReward}
+      </div>
+    </div>
+  );
+}
+
+function ProfileTiers(props: {}) {
+  const [opened, setOpened] = useState(false);
+  return (
+    <div className={tw`flex flex-col w-full gap-2`}>
+      <div
+        className={tw`flex w-full items-center cursor-pointer select-none`}
+        onClick={() => setOpened(!opened)}
+      >
+        <div className={tw`w-full text-sm	`}>Tier level</div>
+        <div className={tw`text-gray-400`}>
+          {!opened ? <HiChevronDown size={20} /> : <HiChevronUp size={20} />}
         </div>
-        <div className={tw`font-semibold`}>{props.pointCount} Points</div>
-        <div className={tw`text-sm opacity-60`}>
-          You have {props.rewardPoints - props.pointCount} points to{" "}
-          {props.nextReward}
-        </div>
-        <div
-          className={tw`flex w-full items-center cursor-pointer`}
-          onClick={() => setOpened(!opened)}
-        >
-          <div className={tw`w-full text-sm	`}>Tier level</div>
-          <div className={tw`text-gray-400`}>
-            {!opened ? <HiChevronDown size={20} /> : <HiChevronUp size={20} />}
-          </div>
-        </div>
-        {opened ? <>
+      </div>
+      {opened ? (
+        <>
           <div className={tw`flex w-full items-center`}>
             <div className={tw`w-full text-sm text-gray-400`}>Bronze level</div>
-            <div className={tw`w-full text-sm text-gray-400 text-right`}>550 Points</div>
+            <div className={tw`w-full text-sm text-gray-400 text-right`}>
+              550 Points
+            </div>
           </div>
           <div className={tw`flex w-full items-center`}>
             <div className={tw`w-full text-sm text-gray-400`}>Silver level</div>
-            <div className={tw`w-full text-sm text-gray-400 text-right`}>1100 Points</div>
+            <div className={tw`w-full text-sm text-gray-400 text-right`}>
+              1100 Points
+            </div>
           </div>
           <div className={tw`flex w-full items-center`}>
             <div className={tw`w-full text-sm text-gray-400`}>Gold level</div>
-            <div className={tw`w-full text-sm text-gray-400 text-right`}>2000 Points</div>
+            <div className={tw`w-full text-sm text-gray-400 text-right`}>
+              2000 Points
+            </div>
           </div>
-        </> : null}
+        </>
+      ) : null}
+    </div>
+  );
+}
+
+interface ProfileSectionProps {
+  pointCount: number;
+  rewardPoints: number;
+  nextReward: string;
+}
+function ProfileSection(props: ProfileSectionProps) {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div
+        className={tw`flex flex-col px-4 py-6 bg-white rounded-xl items-center gap-4`}
+      >
+        <ProfileAccount
+          pointCount={props.pointCount}
+          rewardPoints={props.rewardPoints}
+          nextReward={props.nextReward}
+        />
+        <ProfileTiers />
       </div>
       <BubbleCard
         text="Reserve a spot"
